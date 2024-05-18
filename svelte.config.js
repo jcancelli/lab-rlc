53b9b4dba3c1cc6d39058969b7c879e7c41675ad
jcancelli <jonathan.cancelli.46@gmail.com>
2024-05-18 14:17:55 +0200

    Format and install chart.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,6 +13,6 @@ const config = {
 	paths: {
 		base: process.env.NODE_ENV === "production" ? "lab-rlc" : ""
 	}
-};
+}
 
-export default config;
+export default config
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 export default {
 	content: ["./src/**/*.{html,js,ts,svelte}"],
 	theme: {
-		extend: {},
+		extend: {}
 	},
-	plugins: [],
+	plugins: []
 }
-
